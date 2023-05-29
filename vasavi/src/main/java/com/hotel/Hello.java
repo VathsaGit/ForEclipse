d62209156325c95d04e3354b73a01a2082ed4ed9
DELL <DELL@DESKTOP-QQ99078>
2023-05-29 13:04:58 +0100

    Download and use in Eclipse

---
+++ vasavi/src/main/java/com/hotel/Hello.java
@@ -0,0 +1,25 @@
+package com.hotel;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		arg1.getWriter().print("fasdf");
+	}
+
+}
